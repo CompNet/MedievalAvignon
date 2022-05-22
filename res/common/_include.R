76d7fda54b81cd04e56b43dfc131de8741a02c3d
Vincent Labatut <vince.labatut@gmail.com>
2022-05-22 17:44:01 +0200

    minor adjustments

--- res/common/_include.R
+++ res/common/_include.R
@@ -22,7 +22,7 @@ options(error=recover)		# debug
 #############################################################################################
 # packages
 library("igraph")			# graphs
-library("SDMTools")		# colors
+library("SDMTools")			# colors
 library("CINNA")			# additional centrality measures
 library("future.apply")		# parallel processing
 library("sjstats")			# eta correlation ratio
@@ -33,7 +33,7 @@ library("plotfunctions")	# helper plotting functions
 library("viridis")			# palette
 library("Hmisc")			# fast spearman correlation computation
 library("pcaPP")			# fast kendall correlation computation
-
+library("ggplot2")			# additional plot types
 
 
 
