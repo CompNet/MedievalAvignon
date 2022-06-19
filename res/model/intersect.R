a10b91fd1841ae10c479e85884deb6cc1953eaa6
Vincent Labatut <vince.labatut@gmail.com>
2022-06-19 17:59:56 +0200

    model: debugging street names

--- res/model/intersect.R
+++ res/model/intersect.R
@@ -10,6 +10,112 @@
 
 
 
+###############################################################################
+# Splits the specified edge at the specified point, creating a new vertex and
+# replacing the edge by two new edges. The name of the new edges stay the same.
+#
+# g: city graph.
+# e: edge to split.
+# x,y: position of the new vertex (must be on the edge).
+# name: name of the new vertex.
+# 
+# returns: updated graph.
+###############################################################################
+split.edge <- function(g, v1, v2, x, y, v.name)
+{	# verification
+#	a <- (V(g)[v1]$y - V(g)[v2]$y) / (V(g)[v1]$x - V(g)[v2]$x)
+#	b <- V(g)[v1]$y - a*V(g)[v1]$x
+#	if(y != a*x+b)
+#		stop("ERROR: intersection point (",x,";",y,") not located on edge ",v1,"--",v2)
+	
+	# delete old edge
+	e.name <- E(g)[v1 %--% v2]$name
+	e.type <- E(g)[v1 %--% v2]$type
+	g <- delete_edges(graph=g, edges=E(g)[v1 %--% v2])
+	
+	# add new vertex
+	g <- add_vertices(graph=g, nv=1, attr=list(name=v.name, type="tertiary", x=x, y=y))
+	v3 <- gorder(g)
+	
+	# create new edges
+	g <- add_edges(graph=g, edges=c(v1,v3,v3,v2), attr=list(name=rep(e.name,2), type=rep(e.type,2)))
+	
+	return(g)
+}
+
+
+
+
+###############################################################################
+# Create a path between two vertices, passing through existing vertices.
+#
+# g: graph.
+# start: index of the starting vertex.
+# end: index of the end vertex.
+# e.type: type of the created edges.
+# e.name: name of the created edges.
+# tolerance: maximal angle when selecting next vertex.
+#
+# return: a list containing the completed graph and the created path.
+###############################################################################
+build.path <- function(g, start, end, e.type, e.name, tolerance=30)
+{	u <- start
+	remaining <- (1:gorder(g))[-start]
+	path <- u
+	while(u!=end)
+	{	# compute angle with all remaining vertices
+		angles <- (atan2(V(g)[end]$y - V(g)[u]$y, V(g)[end]$x - V(g)[u]$x) 
+					- atan2(V(g)[remaining]$y - V(g)[u]$y, V(g)[remaining]$x - V(g)[u]$x)) / pi * 180
+		# normalize angles to [0;180]
+		angles[angles>180] <- angles[angles>180] - 360
+		angles[angles<(-180)] <- angles[angles<(-180)] + 360
+		angles <- abs(angles)
+		#print(cbind(remaining,angles))
+		# select smaller angle
+		idx <- which(angles<=tolerance)
+		print(cbind(remaining[idx],angles[idx]))
+		dd <- sapply(remaining[idx], function(v) (V(g)[start]$x-V(g)[v]$x)^2 + (V(g)[start]$y-V(g)[v]$y)^2)
+		v <- remaining[idx[which.min(dd)]]
+		remaining <- remaining[-idx[which.min(dd)]]
+		if(!are.connected(graph=g, v1=u, v2=v))
+			g <- add_edges(graph=g, edges=c(u,v), attr=list(name=e.name, type=e.type))
+		u <- v
+		path <- c(path, u)
+	}
+	
+	res <- list(g=g, path=path)
+	return(res)
+}
+
+
+
+
+###############################################################################
+# Computes the intersection point between the line going through points 1 & 2,
+# and the perpendicular going through point 3.
+# Taken from: https://stackoverflow.com/a/12499474/1254730
+#
+# x1,y1: position of the first point of the line.
+# x2,y2: position of the second point of the line.
+# x3,y3: position of the point of the perpendicular.
+#
+# returns: coordinates of the intersection point.
+###############################################################################
+get.inter.point <- function(x1, y1, x2, y2, x3, y3)
+{	px <- x2 - x1
+	py <- y2 - y1
+	d12 <- px*px + py*py
+	u <- ((x3 - x1) * px + (y3 - y1) * py) / d12;
+	x <- x1 + u * px
+	y <- y1 + u * py
+	
+	res <- c(x,y)
+	return(res)
+}
+
+
+
+
 ############################################################################
 # This function determines if the two specified segments intersect. Each
 # segment is represented by the coordinates of its endpoints. It also returns
@@ -254,7 +360,7 @@ check.link.crossing <- function(g, e=NA)
 				result <- check.segment.crossing(x1, y1, x2, y2, x3, y3, x4, y4)
 #cat("(",n1,",",n2,") vs. (",n3,",",n4,") => ",result,"\n",sep="")
 if(result)
-	cat("Crossing detected for (",n1,",",n2,") vs. (",n3,",",n4,")\n",sep="")
+	tlog(4,"Crossing detected for (",n1,",",n2,") vs. (",n3,",",n4,")")
 			}
 			
 			i <- i + 1		
@@ -281,7 +387,7 @@ if(result)
 add.intersection.nodes <- function(g)
 {	modified <- TRUE
 	
-	# repeat as long as there's a modification in the graph
+	# repeat as long as the graph is modified
 	while(ecount(g)>1 && modified)
 	{	modified <- FALSE
 		i <- 1
@@ -293,7 +399,7 @@ add.intersection.nodes <- function(g)
 			y <- rep(NA,4)
 			
 			# get the link matrix
-			el <- get.edgelist(g)
+			el <- as_edgelist(graph=g, names=FALSE)
 			n[1] <- el[i,1]
 			n[2] <- el[i,2]
 			# get the endpoints coordinates
@@ -324,7 +430,12 @@ add.intersection.nodes <- function(g)
 					
 					# if there's an intersection
 					if(modified)
-					{	cat("Crossing detected for (",n[1],",",n[2],") vs. (",n[3],",",n[4],")\n",sep="")
+					{	tlog(4,"Crossing detected for (",n[1],",",n[2],") vs. (",n[3],",",n[4],")")
+						# get old edge attributes
+						e.type1 <- E(g)[n[1] %--% n[2]]$type
+						e.name1 <- E(g)[n[1] %--% n[2]]$name
+						e.type2 <- E(g)[n[3] %--% n[4]]$type
+						e.name2 <- E(g)[n[3] %--% n[4]]$name
 						
 						# delete both existing links from the graph
 						g <- delete.edges(graph=g,c(E(g)[n[1] %--% n[2]],E(g)[n[3] %--% n[4]]))
@@ -351,8 +462,9 @@ add.intersection.nodes <- function(g)
 								# add 3 new links to the graph, no overlap
 #print(ni)
 								es <- c(ni[1],ni[2],ni[2],ni[3],ni[3],ni[4])
-								dists <- apply(matrix(es,ncol=2,byrow=TRUE), 1, function(c) dist.nodes(g, c[1], c[2]))
-								g <- add.edges(graph=g,edges=es,attr=list(added=TRUE,dist=dists))
+								e.types <- c(e.type1, e.type1, e.type2)
+								e.names <- c(e.name1, e.name1, e.name2)
+								g <- add.edges(graph=g,edges=es,attr=list(added=TRUE, type=e.types, name=e.names))
 							}
 							# case of crossing segments
 							else
@@ -360,11 +472,12 @@ add.intersection.nodes <- function(g)
 								xi <- (d-c)/(a-b)
 								yi <- (a*d-b*c)/(a-b)
 								# add 4 new links to the graph, no crossing
-								g <- add.vertices(graph=g,nv=1,attr=list(added=TRUE,x=xi,y=yi,sx=0,sy=0,ax=0,ay=0,cx=0,cy=0))
+								g <- add.vertices(graph=g,nv=1,attr=list(added=TRUE, x=xi, y=yi, type="tertiary", name="tertiary"))
 								nc <- vcount(g)
 								es <- c(n[1],nc,n[2],nc,n[3],nc,n[4],nc)
-								dists <- apply(matrix(es,ncol=2,byrow=TRUE), 1, function(c) dist.nodes(g, c[1], c[2]))
-								g <- add.edges(graph=g,edges=es,attr=list(added=TRUE,dist=dists))
+								e.types <- c(e.type1, e.type1, e.type2, e.type2)
+								e.names <- c(e.name1, e.name1, e.name2, e.name2)
+								g <- add.edges(graph=g, edges=es, attr=list(added=TRUE, type=e.types, name=e.names))
 							}
 						}
 						
@@ -379,8 +492,9 @@ add.intersection.nodes <- function(g)
 									ni <- n[idx]
 									# add 3 new links to the graph, no overlap
 									es <- c(ni[1],ni[2],ni[2],ni[3],ni[3],ni[4])
-									dists <- apply(matrix(es,ncol=2,byrow=TRUE), 1, function(c) dist.nodes(g, c[1], c[2]))
-									g <- add.edges(graph=g,edges=es,attr=list(added=TRUE,dist=dists))
+									e.types <- c(e.type1, e.type1, e.type2)
+									e.names <- c(e.name1, e.name1, e.name2)
+									g <- add.edges(graph=g,edges=es,attr=list(added=TRUE, type=e.types, name=e.names))
 								}
 								# only the first segment is vertical
 								else
@@ -391,11 +505,12 @@ add.intersection.nodes <- function(g)
 									xi <- x[1]
 									yi <- b*xi + d
 									# add 4 new links to the graph, no crossing
-									g <- add.vertices(graph=g,nv=1,attr=list(added=TRUE,x=xi,y=yi,sx=0,sy=0,ax=0,ay=0,cx=0,cy=0))
+									g <- add.vertices(graph=g,nv=1,attr=list(added=TRUE, x=xi, y=yi, type="tertiary", name="tertiary"))
 									nc <- vcount(g)
 									es <- c(n[1],nc,n[2],nc,n[3],nc,n[4],nc)
-									dists <- apply(matrix(es,ncol=2,byrow=TRUE), 1, function(c) dist.nodes(g, c[1], c[2]))
-									g <- add.edges(graph=g,edges=es,attr=list(added=TRUE,dist=dists))
+									e.types <- c(e.type1, e.type1, e.type2, e.type2)
+									e.names <- c(e.name1, e.name1, e.name2, e.name2)
+									g <- add.edges(graph=g,edges=es,attr=list(added=TRUE, type=e.types, name=e.names))
 								}
 							}
 							# only the second segment is vertical
@@ -407,11 +522,12 @@ add.intersection.nodes <- function(g)
 								xi <- x[3]
 								yi <- a*xi + c
 								# add 4 new links to the graph, no crossing
-								g <- add.vertices(graph=g,nv=1,attr=list(added=TRUE,x=xi,y=yi,sx=0,sy=0,ax=0,ay=0,cx=0,cy=0))
+								g <- add.vertices(graph=g,nv=1,attr=list(added=TRUE, x=xi, y=yi, type="tertiary", name="tertiary"))
 								nc <- vcount(g)
 								es <- c(n[1],nc,n[2],nc,n[3],nc,n[4],nc)
-								dists <- apply(matrix(es,ncol=2,byrow=TRUE), 1, function(c) dist.nodes(g, c[1], c[2]))
-								g <- add.edges(graph=g,edges=es,attr=list(added=TRUE,dist=dists))
+								e.types <- c(e.type1, e.type1, e.type2, e.type2)
+								e.names <- c(e.name1, e.name1, e.name2, e.name2)
+								g <- add.edges(graph=g,edges=es,attr=list(added=TRUE, type=e.types, name=e.names))
 							}
 						}
 					}
@@ -419,6 +535,7 @@ add.intersection.nodes <- function(g)
 				
 				j <- j + 1
 			}
+			
 			i <- i + 1
 		}
 	}
--- res/model/model_streets.R
+++ res/model/model_streets.R
@@ -25,107 +25,6 @@ dir.create(path=out.folder, showWarnings=FALSE, recursive=TRUE)
 
 
 ###############################################################################
-# Splits the specified edge at the specified point, creating a new vertex and
-# replacing the edge by two new edges. The name of the new edges stay the same.
-#
-# g: city graph.
-# e: edge to split.
-# x,y: position of the new vertex (must be on the edge).
-# name: name of the new vertex.
-# 
-# returns: updated graph.
-###############################################################################
-split.edge <- function(g, v1, v2, x, y, v.name)
-{	# verification
-#	a <- (V(g)[v1]$y - V(g)[v2]$y) / (V(g)[v1]$x - V(g)[v2]$x)
-#	b <- V(g)[v1]$y - a*V(g)[v1]$x
-#	if(y != a*x+b)
-#		stop("ERROR: intersection point (",x,";",y,") not located on edge ",v1,"--",v2)
-	
-	# delete old edge
-	e.name <- E(g)[v1 %--% v2]$name
-	e.type <- E(g)[v1 %--% v2]$type
-	g <- delete_edges(graph=g, edges=E(g)[v1 %--% v2])
-	
-	# add new vertex
-	g <- add_vertices(graph=g, nv=1, attr=list(name=v.name, type="tertiary", x=x, y=y))
-	v3 <- gorder(g)
-	
-	# create new edges
-	g <- add_edges(graph=g, edges=c(v1,v3,v3,v2), attr=list(name=rep(e.name,2), type=rep(e.type,2)))
-	
-	return(g)
-}
-
-
-
-
-###############################################################################
-# Create a path between two vertices, passing through existing vertices.
-#
-# g: graph.
-# start: index of the starting vertex.
-# end: index of the end vertex.
-# e.type: type of the created edges.
-# e.name: name of the created edges.
-# tolerance: maximal angle when selecting next vertex.
-#
-# return: a list containing the completed graph and the created path.
-###############################################################################
-build.path <- function(g, start, end, e.type, e.name, tolerance=45)
-{	u <- start
-	remaining <- (1:gorder(g))[-start]
-	path <- u
-	while(u!=end)
-	{	# compute angle with all remaining vertices
-		angles <- (atan2(V(g)[end]$y - V(g)[u]$y, V(g)[end]$x - V(g)[u]$x) 
-			- atan2(V(g)[remaining]$y - V(g)[u]$y, V(g)[remaining]$x - V(g)[u]$x)) / pi * 360
-		idx <- which(abs(angles)<=tolerance)
-		dd <- sapply(remaining[idx], function(v) (V(g)[start]$x-V(g)[v]$x)^2 + (V(g)[start]$y-V(g)[v]$y)^2)
-		v <- remaining[idx[which.min(dd)]]
-		remaining <- remaining[-idx[which.min(dd)]]
-		if(are.connected(graph=g, v1=u, v2=v))
-			v <- end
-		else
-			g <- add_edges(graph=g, edges=c(u,v), attr=list(name=e.name, type=e.type))
-		u <- v
-		path <- c(path, u)
-	}
-	
-	res <- list(g=g, path=path)
-	return(res)
-}
-
-
-
-
-###############################################################################
-# Computes the intersection point between the line going through points 1 & 2,
-# and the perpendicular going through point 3.
-# Taken from: https://stackoverflow.com/a/12499474/1254730
-#
-# x1,y1: position of the first point of the line.
-# x2,y2: position of the second point of the line.
-# x3,y3: position of the point of the perpendicular.
-#
-# returns: coordinates of the intersection point.
-###############################################################################
-get.inter.point <- function(x1, y1, x2, y2, x3, y3)
-{	px <- x2 - x1
-	py <- y2 - y1
-	d12 <- px*px + py*py
-	u <- ((x3 - x1) * px + (y3 - y1) * py) / d12;
-	x <- x1 + u * px
-	y <- y1 + u * py
-	
-	res <- c(x,y)
-	return(res)
-}
-
-
-
-
-###############################################################################
 tlog(0,"Generating a graph similar to Avignon")
 g <- make_empty_graph(n=0, directed=FALSE)
 
@@ -146,7 +45,7 @@ g <- add_vertices(graph=g, nv=n.sec, attr=list(name=paste0("secondary_",1:n.sec)
 # remove those too close
 min.dist <- 0.1
 tlog(2,"Removing close points: tolerance=",min.dist)
-dd <- as.matrix(dist(cbind(V(g)$x,V(g)$y),method="euclidean",diag=FALSE, upper=FALSE))
+dd <- as.matrix(dist(cbind(V(g)$x,V(g)$y), method="euclidean", diag=FALSE, upper=FALSE))
 diag(dd) <- NA
 idx <- which(dd<min.dist,arr.ind=TRUE)
 i <- 0
@@ -165,7 +64,7 @@ while(nrow(idx)>0)
 tlog(4,"Result of the process:")
 tlog(6,"Removed: ",i," points")
 tlog(6,"Remaining: ",gorder(g)," points")
-n.sec <- gorder(g) - 1
+#n.sec <- gorder(g) - 1
 sec.xs <- V(g)$x[-1]
 sec.ys <- V(g)$y[-1]
 #head(sort(c(dd[upper.tri(dd, diag=FALSE)])))
@@ -198,13 +97,13 @@ tlog(4,"Path of the wall: ",paste0(wall,collapsed=", "))
 
 # create avenues
 tlog(2,"Adding radial avenues")
-step <- floor(length(wall) / n.av)
-sq <- seq(1,length(wall),step)
+step <- floor((length(wall)-1) / n.av)
+sq <- seq(1,(length(wall)-1),step)
 tlog(2,"Adding ",length(sq)," radial avenues")
 mids <- c()
 a <- 1
 for(i in sq)
-{	tlog(4,"Adding avenue starting at vertex #",i)
+{	tlog(4,"Adding avenue starting at vertex #",wall[i])
 	tmp <- build.path(g, start=wall[i], end=which(V(g)$name=="center"), e.type="avenue", e.name=paste0("avenue_",a))
 	g <- tmp$g
 	path <- tmp$path
@@ -225,7 +124,6 @@ tlog(2,"Adding minor streets")
 idx <- which(V(g)$type=="secondary")
 V(g)$deg <- sample(x=2:4, size=gorder(g), replace=TRUE)  - degree(graph=g,mode="all")
 V(g)$deg <- sapply(V(g)$deg, function(d) max(0, d))
-z <- 0.5
 changed <- TRUE
 while(changed)
 {	idx <- sample(idx)
@@ -263,7 +161,7 @@ while(changed)
 					# apply modification
 					g <- split.edge(g, v1=el[others[s],1], v2=el[others[s],2], x=end.pts[s,1], y=end.pts[s,2], v.name="tertiary")
 					u <- as.integer(V(g)[gorder(g)])
-					g <- add_edges(graph=g, edges=c(v,u), attr=list(name=paste0("street_",z), type="street"))
+					g <- add_edges(graph=g, edges=c(v,u), attr=list(name=NA, type="street"))
 					z <- z + 1
 					V(g)[u]$deg <- sample(x=0:1, size=1)
 					vrtx <- FALSE
@@ -281,7 +179,7 @@ while(changed)
 				u <- others[which.min(dd)]
 				tlog(6,"Connecting to vertex ",u)
 				# create new edge
-				g <- add_edges(graph=g, edges=c(v,u), attr=list(name=paste0("street_",z), type="street"))
+				g <- add_edges(graph=g, edges=c(v,u), attr=list(name=NA, type="street"))
 				z <- z + 1
 				V(g)[u]$deg <- V(g)[u]$deg - 1
 				changed <- TRUE
@@ -301,18 +199,68 @@ while(changed)
 	}
 }
 
-# TODO: 
-# - nommer les rues à la construction: deg <3 = même nom pr tous les liens attachés si pas déjà un nom?
+# possibly add vertices for intersection points
+tlog(2,"Adding vertices for intersection points")
+g <- add.intersection.nodes(g)
 
-# faut récupérer le graphe dual ?
-# simplifier les noeuds de degré 2 => même rue
+# name unnamed streets
+z <- 1
+unnamed.edges <- which(is.na(E(g)$name))
+while(length(unnamed.edges)>0)
+{	# name next edge
+	str.name <- paste0("street_",z)
+#	el <- as_edgelist(graph=g, names=FALSE)
+	e <- unnamed.edges[1]
+	unnamed.edges <- unnamed.edges[-1]
+	E(g)[e]$name <- str.name
+	
+	# possibly propagate name to adjacent edges
+	inc.edges <- unique(unlist(sapply(incident_edges(graph=g, v=ends(graph=g, es=e, names=FALSE), mode="all"), as.integer)))
+	inc.edges <- setdiff(inc.edges, e)					# remove current edge
+	inc.edges <- intersect(inc.edges, unnamed.edges)	# keep only unnamed edges
+	if(length(inc.edges)>0)
+	{	# keep edge with flatter angle
+		common.ends <- sapply(1:length(inc.edges), function(i) intersect(c(ends(graph=g, es=e, names=FALSE)), c(ends(graph=g, es=inc.edges[i], names=FALSE))))
+		old.ends <- sapply(1:length(inc.edges), function(i) setdiff(c(ends(graph=g, es=e, names=FALSE)), common.ends[i]))
+		new.ends <- sapply(1:length(inc.edges), function(i) setdiff(c(ends(graph=g, es=inc.edges[i], names=FALSE)), common.ends[i]))
+		angles <- (atan2(V(g)[common.ends]$y - V(g)[old.ends]$y, V(g)[common.ends]$x - V(g)[old.ends]$x) 
+					- atan2(V(g)[new.ends]$y - V(g)[common.ends]$y, V(g)[new.ends]$x - V(g)[common.ends]$x)) / pi * 180
+		angles[angles>180] <- angles[angles>180] - 360
+		angles[angles<(-180)] <- angles[angles<(-180)] + 360
+		angles[angles<0] <- -angles[angles<0]
+		e <- inc.edges[which.min(angles)]
+		unnamed.edges <- unnamed.edges[-which(unnamed.edges==e)]
+		E(g)[e]$name <- str.name
+		
+		# try to continue, but vertex-based this time
+		
+	}
+	
+	z <- z + 1
+	#unnamed.edges <- which(is.na(E(g)$name))
+	plot(g, 
+		vertex.label=1:gorder(g),
+#		vertex.label=NA,
+		vertex.color=match(V(g)$type,unique(V(g)$type)), 
+		edge.color=CAT_COLORS_8[match(E(g)$type,unique(E(g)$type))],
+		edge.width=sapply(E(g)$name, function(nm) if(is.na(nm) || nm!=str.name) 1 else 3),
+		vertex.size=3
+	)
+	readline(prompt="Press [enter] to continue")
+}
+
+# TODO: 
+# - faut récupérer le graphe dual ?
+# - simplifier les noeuds de degré 2 => même rue
 
 
 ###############################################################################
 plot(g, 
-	vertex.label=1:gorder(g), 	# 1:gorder(g)
+	vertex.label=1:gorder(g),
+#	vertex.label=NA,
 	vertex.color=match(V(g)$type,unique(V(g)$type)), 
 	edge.color=CAT_COLORS_8[match(E(g)$type,unique(E(g)$type))],
+#	edge.width=sapply(E(g)$name, function(nm) if(is.na(nm)) 1 else 3),
 	vertex.size=3
 )
 write.graph(graph=g, file=file.path(out.folder,"test.graphml"), format="graphml")
