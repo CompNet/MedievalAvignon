6b98caf48185d05882e442a64f300ab24b6edfd7
Vincent Labatut <vince.labatut@gmail.com>
2023-03-25 14:55:18 +0100

    updated readme file

---
+++ res/_install.R
@@ -0,0 +1,26 @@
+#############################################################################################
+# Install the packages required by the R script.
+# 
+# 03/2023 Vincent Labatut
+#
+# source("res/_install.R")
+#############################################################################################
+
+
+
+
+install.packages("igraph")			# graphs
+install.packages("SDMTools")		# colors
+install.packages("CINNA")			# additional centrality measures
+install.packages("future.apply")	# parallel processing
+install.packages("sjstats")			# eta correlation ratio
+install.packages("GoodmanKruskal")	# Goodman-Kruskal tau
+install.packages("DescTools")		# Cramér's V
+install.packages("stringr")			# string operations
+install.packages("plotfunctions")	# helper plotting functions
+install.packages("viridis")			# palette
+install.packages("Hmisc")			# fast spearman correlation computation
+install.packages("pcaPP")			# fast kendall correlation computation
+install.packages("ggplot2")			# additional plot types
+install.packages("geometry")		# convex hull, triangulation
+install.packages("rPref")			# plot pareto front
