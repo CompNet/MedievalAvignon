079cce5ec94364b7305a9bd4e66d5bb3e5c103b7
Vincent Labatut <vince.labatut@gmail.com>
2023-10-02 16:27:16 +0200

    fixed plot pb for full graph

--- res/analysis/extract_graphs.R
+++ res/analysis/extract_graphs.R
@@ -1710,12 +1710,12 @@ info.estate <- info.estate[,-which(colnames(info.estate) %in% c(COL_EST_STREET_I
 		
 		# plot using geographic coordinates
 		g1 <- rescale.coordinates(g1)
-		custom.gplot(g=g1, file=paste0(plot.file,"_lambert"), size.att=2, vertex.label.cex=0.1, rescale=FALSE)
+		custom.gplot(g=as.undirected(g1), file=paste0(plot.file,"_lambert"), size.att=2, vertex.label.cex=0.1, rescale=FALSE)
 		write.graphml.file(g=g1, file=paste0(plot.file,"_lambert.graphml"))
 		
 		# plot using precomputed layout
 		V(g1)$x <- V(g1)$x2; V(g1)$y <- V(g1)$y2
-		custom.gplot(g=g1, file=paste0(plot.file,"_algo"), axes=FALSE, rescale=FALSE, xlim=range(V(g1)$x), ylim=range(V(g1)$y), edge.arrow.mode=0, vertex.label.cex=0.1, size.att=8)
+		custom.gplot(g=as.undirected(g1), file=paste0(plot.file,"_algo"), axes=FALSE, rescale=FALSE, xlim=range(V(g1)$x), ylim=range(V(g1)$y), edge.arrow.mode=0, vertex.label.cex=0.1, size.att=8)
 		write.graphml.file(g=g1, file=paste0(plot.file,"_algo.graphml"))
 		
 		# filter graph by keeping only the main components
@@ -1742,12 +1742,12 @@ info.estate <- info.estate[,-which(colnames(info.estate) %in% c(COL_EST_STREET_I
 		
 		# plot using geographic coordinates
 		g1 <- rescale.coordinates(g1)
-		custom.gplot(g=g1, file=paste0(plot.file,"_lambert"), size.att=2, vertex.label.cex=0.1, rescale=FALSE)
+		custom.gplot(g=as.undirected(g1), file=paste0(plot.file,"_lambert"), size.att=2, vertex.label.cex=0.1, rescale=FALSE)
 		write.graphml.file(g=g1, file=paste0(plot.file,"_lambert.graphml"))
 		
 		# plot using precomputed layout
 		V(g1)$x <- V(g1)$x2; V(g1)$y <- V(g1)$y2
-		custom.gplot(g=g1, file=paste0(plot.file,"_algo"), axes=FALSE, rescale=FALSE, xlim=range(V(g1)$x), ylim=range(V(g1)$y), edge.arrow.mode=0, vertex.label.cex=0.1, size.att=8)
+		custom.gplot(g=as.undirected(g1), file=paste0(plot.file,"_algo"), axes=FALSE, rescale=FALSE, xlim=range(V(g1)$x), ylim=range(V(g1)$y), edge.arrow.mode=0, vertex.label.cex=0.1, size.att=8)
 		write.graphml.file(g=g1, file=paste0(plot.file,"_algo.graphml"))
 	}
 	
@@ -1972,7 +1972,7 @@ info.estate <- info.estate[,-which(colnames(info.estate) %in% c(COL_EST_STREET_I
 				g1 <- rescale.coordinates(g1)
 				custom.gplot(g=g1, file=plot.file, asp=1, size.att=2, vertex.label.cex=0.1, v.hl=v.hl, e.hl=e.hl, rescale=FALSE)
 				
-				# plot using a layouting algorithm 
+				# plot using a layouting algorithm
 				g2 <- g1#; V(g2)$x <- V(g2)$x2; V(g2)$y <- V(g2)$y2
 				graph.folder <- file.path(FOLDER_OUT_ANAL_EST, base.folder, paste0(GR_EST_FLAT_MINUS,"_filtered"), "_removed_streets", "kk")
 				dir.create(path=graph.folder, showWarnings=FALSE, recursive=TRUE)
