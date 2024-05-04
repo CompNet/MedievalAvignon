b3c7f38a8cd69dc2dab960e3e605bf199636f11a
Vincent Labatut <vince.labatut@gmail.com>
2024-05-04 17:14:06 +0200

    added orleans folder

---
+++ res/orleans/extraction.R
@@ -0,0 +1,11 @@
+# Ad hoc script to extract networks from the Orleans data.
+# 
+# Vincent Labatut
+# 05/2024
+#
+# setwd("C:/users/Vincent/Eclipse/workspaces/Extraction/MedievalAvignon")
+# source("res/orleans/extraction.R")
+###############################################################################
+
+
+
